389b7aa902fc4c34ab9122f701f1183a47f675fc
Mizanur Rahman <nickty.86@gmail.com>
2021-08-22 10:23:39 +0600

    primary navigation initialization

--- App.js
+++ App.js
@@ -2,9 +2,21 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import HomeScreen from "./screens/HomeScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import About from "./screens/About";
+import Contact from "./screens/Contact";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <HomeScreen />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Contact">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="About" component={About} />
+        <Stack.Screen name="Contact" component={Contact} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
-
-const styles = StyleSheet.create({});
